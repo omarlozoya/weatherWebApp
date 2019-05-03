cbb946f062fda2ff61b89c98889470413a29890d
Omar Lozoya <23390638+omarlozoya@users.noreply.github.com>
2019-05-03 15:16:17 -0700

    Added seaason display js and css files along with spinner js. These components are used to make the app easier to use and understand.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-const App = () => {
-  return <div>Hello!</div>;
-};
+class App extends React.Component {
+  //   constructor(props) {
+  //     super(props); // super is a reference to the parent's constructer function.
+
+  //     // This is the only time we do direct assignment to our state.
+  //     this.state = { lat: null, errorMsg: "" };
+  //   }
+
+  state = { lat: null, errorMsg: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // we called setState!! to update our state
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => this.setState({ errorMsg: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg} </div>;
+    }
+    if (!this.state.errorMsg && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner msg="Please accept location request" />;
+  }
+
+  // React says we have to define render!! render will be re-rendered when we get our lat from our state.
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
